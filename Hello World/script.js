df028789823ee4d313c0caae29d21be336fbd0fd
Jessica Schubert <jessica8schubert@gmail.com>
2023-12-05 11:26:05 +0100

    testing console.log

---
+++ Hello World/script.js
@@ -0,0 +1,25 @@
+console.log("Hello World!");
+
+//Equal
+console.log(1 == 1);
+//true
+
+//Not Equal
+console.log(1 != 1);
+//false
+
+//Greater than
+console.log(1 > 1);
+//false
+
+// Greater than or equal
+console.log(1 >= 1);
+//true
+
+//Less than
+console.log(1 < 1);
+//false
+
+// Less than or equal
+console.log(1 <= 1);
+//true
