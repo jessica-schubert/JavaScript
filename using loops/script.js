9cce4908d377d4e014904b9bf3490d00e8122315
Jessica Schubert <jessica8schubert@gmail.com>
2023-12-14 10:43:49 +0100

    using loops

--- using loops/script.js
+++ using loops/script.js
@@ -30,8 +30,23 @@ console.log(oddNumbers(9, 12));
  The function has a second parameter which contains the character to search for which is a string.
  Check if the second parameter consists of only one character, if not, quit the function.
  The function returns the number of times the character from the second parameter is present in the word.
- The function should be case insensitive.
+ The function should be case insensitive. */
+
 // TODO: Implement the charCount function
+
+function charCount(word, char) {
+  let charCount = 0;
+  if (char.length !== 1) {
+    return "wrong input";
+  }
+
+  for (let index = 0; index < word.length; index++) {
+    if (word[index].toLowerCase() === char.toLowerCase()) {
+      charCount += 1;
+    }
+  }
+  return charCount;
+}
 
 console.log(charCount("hello", "l"));
 // result should be: 2
